4c312c97fb99c891d70747185667286cfbf80e1b
IanQc <ian.corbin.qc@gmail.com>
2024-03-07 19:40:29 -0500

    1.3c.lp.reste-detail

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+  var swiper = new Swiper(".swiper-screenshots", {
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
