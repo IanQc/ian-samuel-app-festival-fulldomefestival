3eb08e6459dbe8128345d0b7624d24f66ab30904
IanQc <ian.corbin.qc@gmail.com>
2024-03-17 14:39:48 -0400

    13.js.marche

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@
 
 }
 */
-
+/*
 
   var swiperFiltres = new Swiper(".swiper-filters", {
     direction: 'horizontal',
@@ -56,7 +56,7 @@ if (coeurIcon) {
 
 
 
-
+/*
   var swiper = new Swiper(".swiper-screenshots", {
     loop: true,
     centeredSlides: true,
@@ -75,8 +75,45 @@ if (coeurIcon) {
       prevEl: ".swiper-button-prev",
     },
   });
+*/
 
+let swiperlp = document.querySelector(".swiper-screenshots");
+let swiper_app = document.querySelector(".swiper-filters");
 
+if (swiperlp){
+  const premierSwiper = new Swiper (".swiper-screenshots", {
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+} else if (swiper_app){
+  const deuxiemeSwiper = new Swiper (".swiper-filters", {
+    direction: 'horizontal',
+    grabCursor: true,
+    effect: 'slide',
+    spaceBetween: 3
+  });
+  const troisiemeSwiper = new Swiper (".swiper-featured-artworks", {
+    direction: 'horizontal',
+    grabCursor: true,
+    effect: 'slide',
+    slidesPerView: 3,
+    spaceBetween: 3
+  })
+}
 
 
 
